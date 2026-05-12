7d667eca60ada57ef3f571e74b3a936b3a6850db
herna <oscarpitti@gmail.com>
2026-05-12 11:30:22 +0100

    Sprint 2 completado: implementación de arquitectura orientada a eventos con ActiveMQ, productores y consumidores durables, y almacenamiento persistente de eventos mediante event store

--- stocks-module/src/stocks/Main.java
+++ stocks-module/src/stocks/Main.java
@@ -1,22 +1,29 @@
 package stocks;
 
+import com.google.gson.Gson;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        // 1. Crear tabla
-        StockDatabaseInitializer initializer = new StockDatabaseInitializer();
-        initializer.createTable();
-
-        // 2. Obtener datos API
         StockApiClient client = new StockApiClient();
+
         String json = client.fetchQuote("IBM");
 
-        // 3. Parsear JSON
         StockParser parser = new StockParser();
+
         StockQuote quote = parser.parse(json);
 
-        // 4. Guardar en BD
-        StockRepository repository = new StockRepository();
-        repository.save(quote);
+        Event event = new Event("stocks-module", quote);
+
+        Gson gson = new Gson();
+
+        String jsonEvent = gson.toJson(event);
+
+        System.out.println(jsonEvent);
+
+        StockEventPublisher publisher = new StockEventPublisher();
+
+        publisher.publish(jsonEvent);
     }
 }
